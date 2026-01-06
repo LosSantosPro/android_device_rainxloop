e188b2b0808a512fe88f6bafe65e9dee7a0c8e60
LosSantosPro <115103588+LosSantosPro@users.noreply.github.com>
2026-01-06 23:33:33 +0000

    Create fox_rainxloop.mk

---
+++ rainxloop/rainxloop/fox_rainxloop.mk
@@ -0,0 +1,11 @@
+PRODUCT_NAME := fox_rainxloop
+PRODUCT_DEVICE := rainxloop
+PRODUCT_BRAND := generic
+PRODUCT_MODEL := rainxloop
+PRODUCT_MANUFACTURER := generic
+
+# Use recovery.fstab from this tree
+TARGET_RECOVERY_FSTAB := device/rainxloop/rainxloop/recovery.fstab
+
+# Dynamic partitions
+PRODUCT_USE_DYNAMIC_PARTITIONS := true
