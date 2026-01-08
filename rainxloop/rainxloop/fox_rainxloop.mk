8f81b237be9bf4bac4769ad8b7d7e088f83ebd50
LosSantosPro <115103588+LosSantosPro@users.noreply.github.com>
2026-01-08 18:12:45 +0000

    Delete rainxloop/rainxloop directory

--- rainxloop/rainxloop/fox_rainxloop.mk
+++
@@ -1,8 +0,0 @@
-PRODUCT_NAME := fox_rainxloop
-PRODUCT_DEVICE := rainxloop
-PRODUCT_BRAND := generic
-PRODUCT_MODEL := rainxloop
-PRODUCT_MANUFACTURER := generic
-
-# Pull in our device config (creates recovery/root dirs + copies fstab into ramdisk)
-$(call inherit-product, device/rainxloop/rainxloop/rainxloop/device.mk)
