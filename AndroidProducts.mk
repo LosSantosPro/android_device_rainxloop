08285b5ed5c3b942670bd341c59d2e620c288292
LosSantosPro <115103588+LosSantosPro@users.noreply.github.com>
2026-01-08 18:34:38 +0000

    Update AndroidProducts.mk

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -1,3 +1,6 @@
 PRODUCT_MAKEFILES := \
-    $(LOCAL_DIR)/fox_rainxloop.mk \
-    $(LOCAL_DIR)/twrp_rainxloop.mk
+    $(LOCAL_DIR)/twrp_rainxloop.mk \
+    $(LOCAL_DIR)/fox_rainxloop.mk
+
+COMMON_LUNCH_CHOICES := \
+    twrp_rainxloop-eng
