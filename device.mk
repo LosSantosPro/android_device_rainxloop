219fb17480d26933f9f066906558286e6cbecda6
LosSantosPro <115103588+LosSantosPro@users.noreply.github.com>
2026-01-09 08:05:54 +0000

    Update device.mk

--- device.mk
+++ device.mk
@@ -1,6 +1,7 @@
 PRODUCT_USE_DYNAMIC_PARTITIONS := true
 
 PRODUCT_COPY_FILES += \
-    device/rainxloop/rainxloop/recovery.fstab:recovery/root/system/etc/recovery.fstab
+    $(DEVICE_PATH)/recovery.fstab:recovery/root/system/etc/recovery.fstab \
+    $(DEVICE_PATH)/recovery/root/init.recovery.mt6877.rc:recovery/root/init.recovery.mt6877.rc
 
 RECOVERY_SDCARD_ON_DATA := true
