03866bd01f4c3fc84207aae642fe6381bbd7b467
LosSantosPro <115103588+LosSantosPro@users.noreply.github.com>
2026-01-09 06:05:35 +0000

    Update fox_rainxloop.mk

--- fox_rainxloop.mk
+++ fox_rainxloop.mk
@@ -1,8 +1,12 @@
+# device/rainxloop/rainxloop/fox_rainxloop.mk
+
+DEVICE_PATH := device/rainxloop/rainxloop
+
 PRODUCT_NAME := fox_rainxloop
 PRODUCT_DEVICE := rainxloop
 PRODUCT_BRAND := generic
 PRODUCT_MODEL := rainxloop
 PRODUCT_MANUFACTURER := generic
 
-# Pull in our device config (creates recovery/root dirs + copies fstab into ramdisk)
-$(call inherit-product, device/rainxloop/rainxloop/device.mk)
+# Pull in device config (dynamic partitions + fstab copy)
+$(call inherit-product, $(DEVICE_PATH)/device.mk)
