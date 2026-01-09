4aaca4f968a88cafcddf5790472e1a0ca3eaaa94
LosSantosPro <115103588+LosSantosPro@users.noreply.github.com>
2026-01-09 06:31:54 +0000

    Update device.mk

--- device.mk
+++ device.mk
@@ -1,12 +1,12 @@
+DEVICE_PATH := device/rainxloop/rainxloop
+
 # Dynamic partitions
 PRODUCT_USE_DYNAMIC_PARTITIONS := true
 
-# Copy fstab into recovery ramdisk
+# Copy required recovery files into ramdisk
 PRODUCT_COPY_FILES += \
-    $(DEVICE_PATH)/recovery.fstab:recovery/root/system/etc/recovery.fstab
-
-# Optional: keep placeholder dirs (harmless)
-PRODUCT_COPY_FILES += \
+    $(DEVICE_PATH)/recovery.fstab:recovery/root/system/etc/recovery.fstab \
+    $(DEVICE_PATH)/recovery/root/init.recovery.mt6877.rc:recovery/root/init.recovery.mt6877.rc \
     $(DEVICE_PATH)/recovery/root/sbin/.gitkeep:recovery/root/sbin/.gitkeep \
     $(DEVICE_PATH)/recovery/root/system/etc/.gitkeep:recovery/root/system/etc/.gitkeep
 
