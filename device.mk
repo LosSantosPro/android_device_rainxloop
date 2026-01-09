2fbd6ae15a83368567d057ce7e4c478ebd3d3cc1
LosSantosPro <115103588+LosSantosPro@users.noreply.github.com>
2026-01-09 08:05:16 +0000

    Update device.mk

--- device.mk
+++ device.mk
@@ -1,11 +1,6 @@
-# Dynamic partitions
 PRODUCT_USE_DYNAMIC_PARTITIONS := true
 
-# Make sure recovery ramdisk folders exist
 PRODUCT_COPY_FILES += \
-    device/rainxloop/rainxloop/recovery/root/sbin/.gitkeep:recovery/root/sbin/.gitkeep \
-    device/rainxloop/rainxloop/recovery/root/system/etc/.gitkeep:recovery/root/system/etc/.gitkeep \
     device/rainxloop/rainxloop/recovery.fstab:recovery/root/system/etc/recovery.fstab
 
-# Storage behavior
 RECOVERY_SDCARD_ON_DATA := true
