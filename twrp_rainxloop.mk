61b90f86d805540ee1727ca4390ee89878a77a6d
LosSantosPro <115103588+LosSantosPro@users.noreply.github.com>
2026-01-09 06:05:52 +0000

    Update twrp_rainxloop.mk

--- twrp_rainxloop.mk
+++ twrp_rainxloop.mk
@@ -1,3 +1,5 @@
+# device/rainxloop/rainxloop/twrp_rainxloop.mk
+
 # Alias product makefile for builders that expect twrp_<device>
 $(call inherit-product, device/rainxloop/rainxloop/fox_rainxloop.mk)
 
