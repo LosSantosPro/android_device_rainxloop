5a43999ee849b61071fb7fe1367d65e9710636e3
LosSantosPro <115103588+LosSantosPro@users.noreply.github.com>
2026-01-09 08:06:41 +0000

    Update device.mk

--- device.mk
+++ device.mk
@@ -7,8 +7,6 @@ PRODUCT_USE_DYNAMIC_PARTITIONS := true
 PRODUCT_COPY_FILES += \
     $(DEVICE_PATH)/recovery.fstab:recovery/root/system/etc/recovery.fstab \
     $(DEVICE_PATH)/recovery/root/init.recovery.mt6877.rc:recovery/root/init.recovery.mt6877.rc \
-    $(DEVICE_PATH)/recovery/root/sbin/.gitkeep:recovery/root/sbin/.gitkeep \
-    $(DEVICE_PATH)/recovery/root/system/etc/.gitkeep:recovery/root/system/etc/.gitkeep
 
 # Storage
 RECOVERY_SDCARD_ON_DATA := true
